fd223a95256c6697ba49f8b67e2a7de5dd5c924a
h1alexbel <h1alexbelx@gmail.com>
2024-10-01 11:03:25 +0300

    feat(#102): clean

--- sr-data/src/sr_data/steps/label_propagation.py
+++ sr-data/src/sr_data/steps/label_propagation.py
@@ -1,3 +1,27 @@
+"""
+Trains Label Propagation model, and saves it.
+"""
+# The MIT License (MIT)
+#
+# Copyright (c) 2024 Aliaksei Bialiauski
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 import joblib
 import pandas as pd
 from loguru import logger
@@ -8,9 +32,9 @@ from sklearn.semi_supervised import LabelPropagation
 def main(repos, out):
     frame = pd.read_csv(repos)
     frame['label'] = frame['label'].map({'SR': 1, 'NON': 0}).fillna(-1)
-    features = frame.drop(columns=["repo", "label"])
-    scaler = StandardScaler()
-    scaled = scaler.fit_transform(features)
+    scaled = StandardScaler().fit_transform(
+        frame.drop(columns=["repo", "label"])
+    )
     model = LabelPropagation()
     model.fit(scaled, frame["label"])
     joblib.dump(model, out)
