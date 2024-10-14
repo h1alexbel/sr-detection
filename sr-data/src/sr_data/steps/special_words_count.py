4816ad8316228e95a806aa2e6a9071a71938e259
h1alexbel <h1alexbelx@gmail.com>
2024-10-14 12:23:57 +0300

    feat(#138): typo

--- sr-data/src/sr_data/steps/special_words_count.py
+++ sr-data/src/sr_data/steps/special_words_count.py
@@ -1,8 +1,6 @@
 """
-Extract README headings (#).
+Count special words in README.
 """
-import re
-
 # The MIT License (MIT)
 #
 # Copyright (c) 2024 Aliaksei Bialiauski
@@ -24,6 +22,7 @@ import re
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
+import re
 import pandas as pd
 from loguru import logger
 
