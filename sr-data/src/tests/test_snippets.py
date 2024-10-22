0520862882b400d44d0c4437289bac2e1f1a83f2
h1alexbel <h1alexbelx@gmail.com>
2024-10-22 10:39:18 +0300

    feat(#151): test case

---
+++ sr-data/src/tests/test_snippets.py
@@ -0,0 +1,54 @@
+"""
+Tests for snippets.
+"""
+# The MIT License (MIT)
+#
+# Copyright (c) 2024 Aliaksei Bialiauski
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+import unittest
+
+import pytest
+from sr_data.steps.snippets import snippets
+
+
+class TestSnippets(unittest.TestCase):
+
+    @pytest.mark.fast
+    def test_counts_snippets(self):
+        count = snippets("""
+        # test
+        ```bash
+        sh test1.sh
+        ```
+        
+        ```bash
+        sh test2.sh
+        ```
+        
+        ```bash
+        sh test3.sh
+        ```
+        """)
+        expected = 3
+        self.assertEqual(
+            count,
+            expected,
+            f"Found {count} snippets, but it does not match with expected: {expected}"
+        )
