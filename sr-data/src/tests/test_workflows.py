0bed074005a0a1a65a16df82ae5c97535d608b12
h1alexbel <h1alexbelx@gmail.com>
2024-11-21 10:51:26 +0300

    feat(#75): test case for workflow_info

---
+++ sr-data/src/tests/test_workflows.py
@@ -0,0 +1,42 @@
+"""
+Tests for workflows.
+"""
+# The MIT License (MIT)
+#
+# Copyright (c) 2024 Aliaksei Bialiauski
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+import unittest
+
+import pytest
+from sr_data.steps.workflows import workflow_info
+
+
+class TestWorkflows(unittest.TestCase):
+
+    @pytest.mark.fast
+    def test_outputs_workflow_information(self):
+        info = workflow_info(
+            "h1alexbel/h1alexbel/refs/heads/main/.github/workflows/update-readme.yml"
+        )
+        expected = "jobs:"
+        self.assertTrue(
+            expected in info,
+            f"Fetched workflow information: {info} does not contain expected string: {expected}"
+        )
