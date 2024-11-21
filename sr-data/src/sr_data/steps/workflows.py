eb249725c92669b16555d1e29f6fb3f4fd0e2b1a
h1alexbel <h1alexbelx@gmail.com>
2024-11-21 10:02:34 +0300

    feat(#75): yml/yaml workflows

--- sr-data/src/sr_data/steps/workflows.py
+++ sr-data/src/sr_data/steps/workflows.py
@@ -3,13 +3,16 @@ import pandas as pd
 
 def main(repos, out):
     frame = pd.read_csv(repos)
+    frame["workflows"] = frame["workflows"].fillna("")
     for idx, row in frame.iterrows():
         repo = row["repo"]
         branch = row["branch"]
         workflows = row["workflows"]
-        print(workflows)
-        for file in workflows:
-            workflow_info(f"{repo}/{branch}/.github/workflows/{file}")
+        if workflows and not isinstance(workflows, float):
+            for file in workflows.split(","):
+                if file.endswith((".yml", ".yaml")):
+                    workflow_info(f"{repo}/{branch}/.github/workflows/{file}")
+
 
 def workflow_info(path):
     return ""
