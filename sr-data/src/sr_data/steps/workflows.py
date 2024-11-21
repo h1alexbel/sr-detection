1758f24c81fec870c1b1145dac974a17ea075da0
h1alexbel <h1alexbelx@gmail.com>
2024-11-21 10:12:10 +0300

    feat(#75): infos

--- sr-data/src/sr_data/steps/workflows.py
+++ sr-data/src/sr_data/steps/workflows.py
@@ -9,15 +9,20 @@ def main(repos, out):
         repo = row["repo"]
         branch = row["branch"]
         workflows = row["workflows"]
+        infos = []
+        ymls = []
         if workflows and not isinstance(workflows, float):
             ymls = [
                 file.strip()
                 for file in workflows.split(",")
                 if file.endswith((".yml", ".yaml"))
             ]
-            logger.info(f"Repo {repo} has {len(ymls)} *.yml|*.yaml workflows inside .github/workflows")
-            for yml in ymls:
+        logger.info(f"Repo {repo} has {len(ymls)} *.yml|*.yaml workflows inside .github/workflows")
+        for yml in ymls:
+            infos.append(
                 workflow_info(f"{repo}/{branch}/.github/workflows/{yml}")
+            )
+        print(infos)
 
 
 def workflow_info(path):
