7a67e1a6d866a626607007da871024faae5cdc91
h1alexbel <h1alexbelx@gmail.com>
2025-02-04 10:45:25 +0300

    bug(#319): puzzle

--- sr-data/src/sr_data/steps/merge.py
+++ sr-data/src/sr_data/steps/merge.py
@@ -41,6 +41,10 @@ DATASETS = [
 ]
 
 
+# @todo #319:45min Setup tests for merge.
+#  Currently, there is no tests that can catch errors in the datasets merging.
+#  Would be crucial to add setup tests that will check the merging functionality,
+#  and help us catch bugs.
 def main(datasets, out, branch):
     logger.info("Start merging...")
     logger.info(f"Folders={datasets}")
